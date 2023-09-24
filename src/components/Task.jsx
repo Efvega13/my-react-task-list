2cb155085ecb7f60a1dbe6748e45155a81819aeb
Elio Vega <eliosilver@hotmail.com>
2023-09-23 20:47:16 -0500

    cambio project 4

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,61 +1,78 @@
-import {useState} from 'react'
+import { useState } from 'react';
 
-
-function Task({ name, status, onStatusChange, onDelete, onUpdate}) {
+function Task({ name, description, status, onStatusChange, onDelete, onUpdate }) {
   const [isEditing, setIsEditing] = useState(false);
   const [updatedTaskName, setUpdatedTaskName] = useState(name);
+  const [updatedTaskDescription, setUpdatedTaskDescription] = useState(description);
 
   const handleCheckboxChange = () => onStatusChange();
-  
+
   const handleDelete = () => onDelete();
 
   const handleEditClick = () => setIsEditing(true);
 
   const handleCancelClick = () => {
     setIsEditing(false);
-    setUpdatedTaskName(name)
-  }
+    setUpdatedTaskName(name);
+    setUpdatedTaskDescription(description);
+  };
 
   const handleSaveClick = () => {
-    if (updatedTaskName.trim() !== ''){
-      onUpdate(updatedTaskName);
+    if (updatedTaskName.trim() !== '') {
+      onUpdate(updatedTaskName, updatedTaskDescription);
       setIsEditing(false);
     }
-  }
+  };
 
   const handleInputChange = (e) => {
     setUpdatedTaskName(e.currentTarget.value);
-  }
+  };
+
+  const handleDescriptionChange = (e) => {
+    setUpdatedTaskDescription(e.currentTarget.value);
+  };
 
   return (
     <div className='contenedor'>
-      <input 
-          type='checkbox'
-          checked={ status }
-          onChange={ handleCheckboxChange }
-       />
-       {isEditing ? (
-        <input 
-            type='text' 
-            value={ updatedTaskName }
-            onChange={ handleInputChange }
-        />
-       ) : (
-        <p className={`Tarea ${status ? 'Tarea-tachada' : ''}`}>{name} </p>     
-       )}
-       {isEditing ? (
+      <input
+        type='checkbox'
+        checked={status}
+        onChange={handleCheckboxChange}
+      />
+      {isEditing ? (
         <>
-            <button onClick={ handleSaveClick }> Guardar </button>
-            <button onClick={ handleCancelClick }> Cancelar </button>
+          <input
+            type='text'
+            value={updatedTaskName}
+            onChange={handleInputChange}
+          />
+          <input
+            type='text'
+            value={updatedTaskDescription}
+            onChange={handleDescriptionChange}
+          />
         </>
-       ):(
+      ) : (
         <>
-            <button onClick={ handleEditClick }> Editar </button>
-            <button onClick={ handleDelete }> Borrar </button>
+          <p className={`Tarea ${status ? 'Tarea-tachada' : ''}`}>{name}</p>
+          <p className={`Tarea ${status ? 'Tarea-tachada' : ''}`}>
+            {description}
+          </p>
         </>
-       )}
+      )}
+      {isEditing ? (
+        <>
+          <button onClick={handleSaveClick}>Guardar</button>
+          <button onClick={handleCancelClick}>Cancelar</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleEditClick}>Editar</button>
+          <button onClick={handleDelete}>Borrar</button>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Task
+export default Task;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,75 +1,95 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import useAdminTarea from '../hooks/useAdminTarea';
 import Task from './Task';
 
-
 function TaskList() {
-  const { tasks, createTask, deleteTask, updateTask, updateTaskStatus } = useAdminTarea();
+  const { tasks, createTask, deleteTask, updateTask, updateTaskStatus } =
+    useAdminTarea();
   const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
   const [pendingTasks, setPendingTasks] = useState(0);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    setPendingTasks(tasks.filter((task) => !task.status).length)}, [tasks])
+    setPendingTasks(tasks.filter((task) => !task.status).length);
+  }, [tasks]);
 
-  const handleTaskStatusChange = (index) => updateTaskStatus(index)
+  const handleTaskStatusChange = (index) => updateTaskStatus(index);
 
   const addTask = (e) => {
     e.preventDefault();
-    if(taskName.trim() !== ''){
-      createTask(taskName)
-      setTaskName('')
+    if (taskName.trim().length >= 3) {
+      createTask(taskName, taskDescription);
+      setTaskName('');
+      setTaskDescription('');
+      setError('');
+    } else {
+      setError('El nombre de la tarea debe tener al menos 3 caracteres.');
     }
-  }
+  };
 
-  const handleDeleteTask = (index) => deleteTask(index)
+  const handleDeleteTask = (index) => deleteTask(index);
 
-  const handleUpdateTask = (index, updateName) => updateTask(index, {name: updateName})
+  const handleUpdateTask = (index, updateName, updateDescription) =>
+    updateTask(index, { name: updateName, description: updateDescription });
 
-  const handleInputChange = (e) => setTaskName(e.target.value)
+  const handleInputChange = (e) => setTaskName(e.target.value);
 
-  const handleClearAll = () =>{
+  const handleDescriptionChange = (e) => setTaskDescription(e.target.value);
+
+  const handleClearAll = () => {
     const completedTaskIndexes = tasks.reduce((indexes, task, index) => {
-      if(task.status)
-        indexes.push(index)
-      return indexes
-    }, [])
+      if (task.status) indexes.push(index);
+      return indexes;
+    }, []);
     completedTaskIndexes.reverse().forEach((index) => {
-      deleteTask(index)
-    })
-  }
+      deleteTask(index);
+    });
+  };
 
   return (
     <>
-      <form onSubmit={ addTask }>
-        <input 
-          type="text" 
-          placeholder='Add new task'
-          value={ taskName }
-          onChange={ handleInputChange }
-          />
+      <form onSubmit={addTask}>
+        <input
+          type='text'
+          placeholder='Task Name'
+          value={taskName}
+          onChange={handleInputChange}
+        />
+        <input
+          type='text'
+          placeholder='Task Description (optional)'
+          value={taskDescription}
+          onChange={handleDescriptionChange}
+        />
         <button>Add</button>
+        {error && <p className='error'>{error}</p>}
       </form>
       <ul>
         {tasks.map((task, index) => (
-          <li key={ index }>
-            <Task 
-              name={task.name} 
+          <li key={index}>
+            <Task
+              name={task.name}
+              description={task.description}
               status={task.status}
               onStatusChange={() => handleTaskStatusChange(index)}
-              onDelete={()=> handleDeleteTask(index)}
-              onUpdate={(updatedName) => handleUpdateTask(index, updatedName)}
+              onDelete={() => handleDeleteTask(index)}
+              onUpdate={(updatedName, updatedDescription) =>
+                handleUpdateTask(index, updatedName, updatedDescription)
+              }
             />
           </li>
         ))}
       </ul>
       <>
-          <p> You have { pendingTasks } pending tasks. </p>
+        <p>You have {pendingTasks} pending tasks.</p>
       </>
       <div className='borrar-contenedor'>
-          <button onClick={handleClearAll}> Clean completed tasks </button>
+        <button onClick={handleClearAll}>Clean completed tasks</button>
       </div>
     </>
-  )
+  );
 }
 
-export default TaskList
+export default TaskList;
+
